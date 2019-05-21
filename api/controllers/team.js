30a28d6cde29e881b4578002382dcf9dd0b0519a
Mehmet Sefa Balık <sefa.balik@trendyol.com>
2019-05-21 21:42:28 +0300

    mock data is added

--- api/controllers/team.js
+++ api/controllers/team.js
@@ -2,12 +2,19 @@ const teamService = require('../services/team');
 
 module.exports.create = async (request, reply) => {
     const team = await teamService.create(request.body.name);
-    return reply.code(200).send({ team });
+    return reply.code(200).send({ team: {name: 'mobil web'} });
 };
 
 module.exports.getAll = async (request, reply) => {
-    const team = await teamService.getAll();
-    return reply.code(200).send({ team });
+    const teams = await teamService.getAll();
+    return reply.code(200).send({ teams: [
+            {name: 'Mobil Web'},
+            {name: 'Web'},
+            {name: 'Browsing'},
+            {name: 'Checkout'},
+            {name: 'System'},
+        ]
+    });
 };
 
 module.exports.delete = async (request, reply) => {
